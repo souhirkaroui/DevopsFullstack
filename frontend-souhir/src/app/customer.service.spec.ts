76251a40a243fd70a53081f9b919df68ce4296e6
souhirkaroui <souhir.karoui.ks@gmail.com>
2026-02-24 01:56:46 +0100

    Update customer.service.spec.ts

--- frontend-souhir/src/app/customer.service.spec.ts
+++ frontend-souhir/src/app/customer.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // <-- pour simuler HttpClient
 import { CustomerService } from './customer.service';
 
 describe('CustomerService', () => {
   let service: CustomerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule] // <-- ajoute ça
+    });
     service = TestBed.inject(CustomerService);
   });
 
