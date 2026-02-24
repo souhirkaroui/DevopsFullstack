0b04622da6b4f2b1b936753b62414110ffc1af93
souhirkaroui <souhir.karoui.ks@gmail.com>
2026-02-24 16:41:46 +0100

    Create material.module.ts

---
+++ frontend-souhir/src/app/material.module.ts
@@ -0,0 +1,18 @@
+// src/app/material.module.ts
+
+import { NgModule } from '@angular/core';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule {}
