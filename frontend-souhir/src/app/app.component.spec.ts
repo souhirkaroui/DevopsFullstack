ae1de567a74e9169e88b2a6f141f133f14c752e0
souhirkaroui <souhir.karoui.ks@gmail.com>
2026-02-24 01:55:01 +0100

    Update app.component.spec.ts

--- frontend-souhir/src/app/app.component.spec.ts
+++ frontend-souhir/src/app/app.component.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CustomerService } from './customer.service'; // <-- ajoute ton service si utilisé
+import { HttpClientModule } from '@angular/common/http'; // <-- indispensable pour HttpClient
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule // <-- permet l'injection de HttpClient
+      ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        CustomerService // <-- ajoute tous les services utilisés par ton composant
+      ]
     }).compileComponents();
   });
 
