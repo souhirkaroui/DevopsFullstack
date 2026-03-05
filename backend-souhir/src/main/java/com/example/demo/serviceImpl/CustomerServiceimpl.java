7a60fd117a5f505a315e2d9acba0cd7dbc4be8c7
souhirkaroui <souhir.karoui.ks@gmail.com>
2026-03-06 00:29:56 +0100

    Update and rename CustomerServiceImpl.java to CustomerServiceimpl.java

--- backend-souhir/src/main/java/com/example/demo/serviceImpl/CustomerServiceimpl.java
+++ backend-souhir/src/main/java/com/example/demo/serviceImpl/CustomerServiceimpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
-public class CustomerServiceImpl implements CustomerService {
+public class CustomerServiceimpl implements CustomerService {
 
     @Autowired
     private CustomerRepository repository;
