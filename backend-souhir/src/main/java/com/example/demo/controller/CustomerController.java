10b9f7f88e31c5aa85852e63ec45d27aa934159d
souhirkaroui <souhir.karoui.ks@gmail.com>
2026-03-07 01:49:03 +0100

    Update CustomerController.java

--- backend-souhir/src/main/java/com/example/demo/controller/CustomerController.java
+++ backend-souhir/src/main/java/com/example/demo/controller/CustomerController.java
@@ -4,12 +4,27 @@ import com.example.demo.entity.Customer;
 import com.example.demo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://myapp.local")
-@RestController
-@RequestMapping("/api/customers")
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/api/**")
+                    .allowedOrigins("https://fullstack.ghazelatech.com")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE");
+        }
+    };
+}
+
+
+
 public class CustomerController {
 
     @Autowired
